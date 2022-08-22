b973f1b7a6b8e45fcfaf4e808803fcae8638c9a4
Prunthil Kheni <95540748+prunthil20@users.noreply.github.com>
2022-08-22 23:01:35 +0530

    Add files via upload

---
+++ IncDec.dart
@@ -0,0 +1,18 @@
+void main() {
+  var a = 10;
+  var p = 10;
+
+  // increment
+  var b = a++;
+  print(b);
+
+  var c = ++b;
+  print(c);
+
+  // Decrement
+  var d = p--;
+  print(d);
+
+  var e = --d;
+  print(e);
+}
