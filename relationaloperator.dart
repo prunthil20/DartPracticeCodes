7b62670a68618487fd72c57acc8cb8e9bbb647a7
Prunthil Kheni <95540748+prunthil20@users.noreply.github.com>
2022-08-22 23:07:46 +0530

    Add files via upload

---
+++ relationaloperator.dart
@@ -0,0 +1,16 @@
+void main() {
+  var a = 10;
+  var b = 20;
+  var ans = a > b;
+  print("a is greater than b : $ans");
+  ans = a < b;
+  print("a is lessS than b : $ans");
+  ans = a >= b;
+  print("a is greater than equal to b : $ans");
+  ans = a <= b;
+  print("a is lesser than equal to b : $ans");
+  ans = a == b;
+  print("a is equal to b : $ans");
+  ans = a != b;
+  print("a is not equal to b : $ans");
+}
